25a426a32c42b29c13d2fb30d97083d831ba402b
unknown <thompson.jonm@gmail.com>
2017-11-09 19:12:30 -0600

    selenium inop. check work terminal for correct path. cannot find bindary. no docs.

---
+++ tests/seleniumTests/pigmaticDriver.test.js
@@ -0,0 +1,14 @@
+var webdriver = require('selenium-webdriver');
+var chrome = require('selenium-webdriver/chrome');
+var path = require('chromedriver').path;
+
+var service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
+
+var driver = new webdriver.Builder()
+    .withCapabilities(webdriver.Capabilities.chrome())
+    .build();
+
+driver.get('https://pigmatic-app.herokuapp.com');
+driver.findElement(webdriver.By.linkText('/add-collection')).click();
+driver.quit();
