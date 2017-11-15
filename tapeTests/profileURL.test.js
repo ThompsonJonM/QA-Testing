e579608adb4b3dc0b56f91c4c99ca3b12136dae7
unknown <thompson.jonm@gmail.com>
2017-11-14 18:13:11 -0600

    added new test for test case

--- tapeTests/profileURL.test.js
+++ tapeTests/profileURL.test.js
@@ -1,10 +1,9 @@
 const test = require('tape');
-const beforeEach = require('tape');
-const afterEach = require('tape');
-
 const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const path = require('chromedriver').path;
+const beforeEach = require('tape');
+const afterEach = require('tape');
 
 const service = new chrome.ServiceBuilder(path).build()
 chrome.setDefaultService(service);
@@ -18,27 +17,40 @@ beforeEach('should open to profile page', function(t) {
     t.end();
 })
 
-test('should enter text into contact and submit', function(t) {
-    driver.findElement(webdriver.By.name('contactLink')).click();
-    driver.findElement(webdriver.By.name('name')).sendKeys('Jonathan');
-    driver.findElement(webdriver.By.name('email')).sendKeys('test case');
-    driver.findElement(webdriver.By.name('message')).sendKeys('This should fail.');
-    driver.findElement(webdriver.By.name('submitButton')).click();
-    t.fail('Invalid input entered');
+test('should verify empty title', function(t) {
+    driver.getTitle().then(function(title) {
+        t.equal(title, '');
+    })
+    t.end();
+});
 
+test('should go to contact page', function(t) {
+    driver.findElement(webdriver.By.name('contactLink')).click();
+    driver.getCurrentUrl().then(function(url) {
+        t.equal(url, 'http://www.jmarshthompson.com/contact');
+    })
+    t.end();
+})
+
+test('should go to the works page', function(t) {
+    driver.findElement(webdriver.By.name('worksLink')).click();
+    driver.getCurrentUrl().then(function(url) {
+        t.deepEqual(url, 'http://www.jmarshthompson.com/works');
+    })
     t.end();
 })
 
 test('should enter text into contact and submit', function(t) {
     driver.findElement(webdriver.By.name('contactLink')).click();
-    driver.findElement(webdriver.By.name('name')).sendKeys('Jonathan');
+    driver.findElement(webdriver.By.name('name')).sendKeys('Jonathan Thompson');
     driver.findElement(webdriver.By.name('email')).sendKeys('jthompson@ricasurgical.com');
-    driver.findElement(webdriver.By.name('message')).sendKeys('This should pass.');
+    driver.findElement(webdriver.By.name('message')).sendKeys('This is a test using Selenium and Tape.');
     driver.findElement(webdriver.By.name('submitButton')).click();
     driver.getCurrentUrl().then(function(url) {
         t.equal(url, 'http://www.jmarshthompson.com/contact-submit');
-    })
 
+    driver.quit();
+    })
     t.end();
 })
 
